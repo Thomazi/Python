e72eabf5d22ab9e1e7fde4f3118e6554a2e9c100
Gabriel Thomazi Rosa <gabrielthomazirosa@outlook.com>
2024-01-02 16:45:02 -0300

    Aprendizado Python

--- exercicios/ex005.py
+++
@@ -1,8 +0,0 @@
-#crie um programa que ordene uma lista de números em ordem crescente ou decrescente.
-
-lista = [5,8,2,3,8,42,11,36,4]
-
-lista_crescente = sorted(lista)
-lista_decrescente = sorted(lista, reverse=True)
-
-print("Lista crescente: {}\nLista decrescente: {}".format(lista_crescente, lista_decrescente))
